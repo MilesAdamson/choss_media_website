3b8d23d213859d3cfc69267c4060537039b4792a
MilesAdamson <milesadamson@hotmail.com>
2020-01-19 22:48:28 -0700

    card

--- lib/entries/photo_entries/photo_entry.dart
+++ lib/entries/photo_entries/photo_entry.dart
@@ -14,43 +14,46 @@ class PhotoEntry extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  title,
-                  style: TextStyle(color: Colors.black, fontSize: 32.0),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 8.0),
-                  child: Text(
-                    flavourText,
-                    style: TextStyle(color: Colors.black, fontSize: 16.0),
+    return Card(
+      elevation: 4.0,
+      child: Padding(
+        padding: EdgeInsets.all(32.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    title,
+                    style: TextStyle(color: Colors.black, fontSize: 32.0),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(bottom: 2.0),
-                  child: Text(
-                    'Taken By: $takenBy',
-                    style: TextStyle(color: Colors.grey, fontSize: 14.0),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 8.0),
+                    child: Text(
+                      flavourText,
+                      style: TextStyle(color: Colors.black, fontSize: 16.0),
+                    ),
                   ),
-                ),
-              ],
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 2.0),
+                    child: Text(
+                      'Taken By: $takenBy',
+                      style: TextStyle(color: Colors.grey, fontSize: 14.0),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          Image.network(
-            assetPath + '.jpg',
-            width: MediaQuery.of(context).size.width,
-            fit: BoxFit.fitHeight,
-          ),
-        ],
+            Image.network(
+              assetPath + '.jpg',
+              width: MediaQuery.of(context).size.width,
+              fit: BoxFit.fitHeight,
+            ),
+          ],
+        ),
       ),
     );
   }
