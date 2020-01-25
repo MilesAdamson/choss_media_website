d3034249d3904d6d1ad77ffa510a80eea4fdd014
MilesAdamson <milesadamson@hotmail.com>
2020-01-25 12:24:34 -0700

    fix for mobile?>

--- lib/components/complete_widgets/athlete_card.dart
+++ lib/components/complete_widgets/athlete_card.dart
@@ -1,77 +1,77 @@
 import 'package:choss_media/components/base/content_card.dart';
 import 'package:choss_media/components/base/title_row.dart';
+import 'package:choss_media/components/base/title_text.dart';
 import 'package:choss_media/entries/athlete_entries.dart';
+import 'package:choss_media/util/util.dart';
 import 'package:flutter/material.dart';
 
 import 'dart:html' as html;
 
-class AthleteCard extends StatelessWidget {
+class AthleteCard extends StatefulWidget {
   final AthleteEntry athleteEntry;
 
   AthleteCard({@required this.athleteEntry});
 
   @override
+  State<StatefulWidget> createState() => AthleteCardState();
+}
+
+class AthleteCardState extends State<AthleteCard> {
+  bool get landscape => MediaQuery.of(context).size.width > 1000.0;
+
+  @override
   Widget build(BuildContext context) {
     return ContentCard(
-      child: Wrap(
-        direction: Axis.horizontal,
-        children: <Widget>[
-          Image.network(
-            athleteEntry.photoPath,
-            height: 500,
-            width: 500,
-            fit: BoxFit.fitWidth,
-            alignment: Alignment.center,
-          ),
-          Container(
-            width: 400,
-            child: Column(
+      child: landscape
+          ? Row(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                TitleRow(
-                  fractionWidth: 0.6,
-                  title: athleteEntry.name,
-                  detail: athleteEntry.nickname,
-                  avatarPaths: <AthleteEntry>[],
-                ),
-                Divider(),
-                Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Text(athleteEntry.fullBio, style: TextStyle(fontSize: 14.0),),
-                )
-              ],
+              children: children())
+          : Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: children(),
             ),
-          ),
-        ],
-      ),
     );
   }
 
-  Widget profilePic(BuildContext context) {
-    return Container(
-      height: 500,
-      width: 500,
-      child: Stack(
+  List<Widget> children() {
+    double size = Util.cap((MediaQuery.of(context).size.width / 1000.0) * 700,
+        max: 500, min: 100);
+    return <Widget>[
+      Image.network(
+        widget.athleteEntry.photoPath,
+        height: size,
+        width: size,
+        fit: BoxFit.fitWidth,
+        alignment: Alignment.center,
+      ),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Image.network(
-            athleteEntry.photoPath,
-            height: 500,
-            width: 500,
-            fit: BoxFit.fitWidth,
-            alignment: Alignment.center,
+          TitleRow(
+            fractionWidth: landscape ? 0.5 : 0.8,
+            title: widget.athleteEntry.name,
+            detail: widget.athleteEntry.nickname,
+            avatarPaths: <AthleteEntry>[],
           ),
-          Positioned(
-            right: 4,
-            bottom: 4,
-            child: instaButton(context),
-          ),
+          Container(
+              width:  landscape ? MediaQuery.of(context).size.width * 0.5 : MediaQuery.of(context).size.width * 0.9,
+              child: Divider()),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: SizedText(
+              widget.athleteEntry.fullBio,
+              max: 14.0,
+              min: 11.0,
+              fractionWidth: 0.5,
+            ),
+          )
         ],
       ),
-    );
+    ];
   }
 
   Widget instaButton(BuildContext context) {
-    if (athleteEntry.igURL == null) {
+    if (widget.athleteEntry.igURL == null) {
       return Container();
     }
     double size = MediaQuery.of(context).size.width * 0.03;
@@ -79,7 +79,7 @@ class AthleteCard extends StatelessWidget {
       size = 25.0;
     }
     return GestureDetector(
-      onTap: () => html.window.open(athleteEntry.igURL, "name?"),
+      onTap: () => html.window.open(widget.athleteEntry.igURL, "name?"),
       child: Image.network(
         '/assets/insta.png',
         height: size,
