136d7c3f996e180ab4f032c08db6d05d8a068f5b
MilesAdamson <milesadamson@hotmail.com>
2020-01-24 21:42:57 -0700

    force athlete card to be 400

--- lib/components/complete_widgets/athlete_card.dart
+++ lib/components/complete_widgets/athlete_card.dart
@@ -24,7 +24,7 @@ class AthleteCard extends StatelessWidget {
             alignment: Alignment.center,
           ),
           Container(
-            width: MediaQuery.of(context).size.width * 0.4,
+            width: 400,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
