9af0bcd7a67b2d8027703c156ef2941617259ebd
MilesAdamson <milesadamson@hotmail.com>
2020-01-26 16:28:40 -0700

    now fixed apop

--- lib/components/complete_widgets/app_card.dart
+++ lib/components/complete_widgets/app_card.dart
@@ -29,7 +29,7 @@ class AppCardState extends State<AppCard> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: children())
           : Container(
-              height: MediaQuery.of(context).size.height * 1.3,
+              height: MediaQuery.of(context).size.height * 1.4,
               child: Column(
                 children: children(),
               ),
@@ -60,20 +60,19 @@ class AppCardState extends State<AppCard> {
                 alignment: Alignment.center,
               ),
             ),
-            SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: Material(
-                elevation: 3.0,
-                child: Container(
-                  padding: EdgeInsets.all(8.0),
-                  width: MediaQuery.of(context).size.width *
-                      (landscape ? 0.5 : 0.9),
-                  height: 400,
-                  child: Padding(
-                    padding: EdgeInsets.only(
-                        bottom: landscape ? 64.0 : 16.0, top: 16.0),
+            Material(
+              elevation: 3.0,
+              child: Container(
+                padding: EdgeInsets.all(8.0),
+                width: MediaQuery.of(context).size.width *
+                    (landscape ? 0.5 : 0.9),
+                height: 400,
+                child: Padding(
+                  padding: EdgeInsets.only(
+                      bottom: landscape ? 64.0 : 16.0, top: 16.0),
+                  child: SingleChildScrollView(
                     child: SizedText(
-                      widget.appEntry.description,
+                      widget.appEntry.description + widget.appEntry.description + widget.appEntry.description,
                       max: 16.0,
                       min: 12.0,
                       fractionWidth: landscape ? 0.5 : 0.9,
@@ -82,7 +81,10 @@ class AppCardState extends State<AppCard> {
                 ),
               ),
             ),
-            getItOnGooglePlay(),
+            Padding(
+              padding: EdgeInsets.only(top: 16.0),
+              child: getItOnGooglePlay(),
+            ),
           ],
         ),
       ),
