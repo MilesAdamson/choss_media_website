690cf574515832d85052d7fb377d2197f208dbb2
MilesAdamson <milesadamson@hotmail.com>
2020-01-27 20:54:30 -0700

    video max width

--- lib/components/complete_widgets/video_card.dart
+++ lib/components/complete_widgets/video_card.dart
@@ -12,42 +12,48 @@ class VideoCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ContentCard(
-      child: Column(
-        children: <Widget>[
-          if (MediaQuery.of(context).size.width > 700)
-            TitleRow(
-              title: videoEntry.title,
-              flavourText: videoEntry.flavourText,
-              avatarPaths: videoEntry.athletes.map((a) => a).toList(),
-              fractionWidth: 0.5,
-            )
-          else
-            Column(
-              children: <Widget>[
+    return Center(
+      child: Container(
+        constraints: BoxConstraints(
+          maxWidth: 1200
+        ),
+        child: ContentCard(
+          child: Column(
+            children: <Widget>[
+              if (MediaQuery.of(context).size.width > 700)
                 TitleRow(
                   title: videoEntry.title,
                   flavourText: videoEntry.flavourText,
-                  fractionWidth: 0.8,
-                ),
-                TitleRow(
-                  fractionWidth: 0.1,
                   avatarPaths: videoEntry.athletes.map((a) => a).toList(),
-                  title: " ",
+                )
+              else
+                Column(
+                  children: <Widget>[
+                    TitleRow(
+                      title: videoEntry.title,
+                      flavourText: videoEntry.flavourText,
+                      fractionWidth: 0.8,
+                    ),
+                    TitleRow(
+                      fractionWidth: 0.1,
+                      avatarPaths: videoEntry.athletes.map((a) => a).toList(),
+                      title: " ",
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          Stack(
-            children: <Widget>[
-              Image.network(
-                videoEntry.path,
-                width: MediaQuery.of(context).size.width,
-                fit: BoxFit.fitWidth,
+              Stack(
+                children: <Widget>[
+                  Image.network(
+                    videoEntry.path,
+                    width: MediaQuery.of(context).size.width,
+                    fit: BoxFit.fitWidth,
+                  ),
+                  centeredPlayButton(context)
+                ],
               ),
-              centeredPlayButton(context)
             ],
           ),
-        ],
+        ),
       ),
     );
   }
